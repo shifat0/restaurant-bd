a310b646272e81d196a1c7ecbf01a09dd58c81a0
Md. Shoaib Akhter Shifat <sifuisu3@gmail.com>
2023-11-09 16:11:38 +0600

    redux-logger

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,6 +1,7 @@
-import { legacy_createStore as createStore } from "redux";
+import { legacy_createStore as createStore, applyMiddleware } from "redux";
 import { Reducer } from "./Reducer";
+import logger from "redux-logger";
 
-const store = createStore(Reducer);
+const store = createStore(Reducer, applyMiddleware(logger));
 
 export default store;
