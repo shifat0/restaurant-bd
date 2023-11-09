64a89a98cd21bb09a45368225ab7d6fa1690b2be
Md. Shoaib Akhter Shifat <sifuisu3@gmail.com>
2023-11-09 13:54:59 +0600

    combining reducers

--- src/Redux/Reducer.js
+++ src/Redux/Reducer.js
@@ -1,19 +1,21 @@
 import DISHES from "../data/dishes";
 import COMMENTS from "../data/comments";
+import { combineReducers } from "redux";
 
-const initialState = {
-  dishes: DISHES,
-  comments: COMMENTS,
+const dishReducer = (state = DISHES, action) => {
+  return state;
 };
 
-export const Reducer = (state = initialState, action) => {
+const commentReducer = (state = COMMENTS, action) => {
   if (action.type === "ADD_COMMENT") {
     let comment = action.payload;
-    comment.id = state.comments.length;
-    return {
-      ...state,
-      comments: state.comments.concat(comment),
-    };
+    comment.id = state.length;
+    return state.concat(comment);
   }
   return state;
 };
+
+export const Reducer = combineReducers({
+  dishes: dishReducer,
+  comments: commentReducer,
+});
